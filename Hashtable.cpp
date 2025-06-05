9ec044fd6ac549866b13b38675885a8d426b6a75
palmer29 <cambuchamushroom@gmail.com>
2025-06-05 14:08:56 +0300

    Update Hashtable.cpp

--- Hashtable.cpp
+++ Hashtable.cpp
@@ -1,3 +1,63 @@
+#include "HashTable.h"
+#include <vector>
+#include <cstdlib>
+
+
+HashTable::HashTable(size_t size) noexcept : _capacity(size), _filled(0)
+{
+    table.resize(_capacity);
+}
+
+size_t HashTable::hash_function(const KeyType &key) const
+{
+    size_t res = 0;
+    for( const auto& i: key)
+    {
+	res = (res + i) % _capacity;
+    }
+    return static_cast<size_t>(res); 
+} 
+
+HashTable::~HashTable()
+{}
+
+void HashTable::insert(const KeyType &key, const ValueType &value)
+{
+    if (getLoadFactor() > 0.75)
+    {
+        _capacity *= 2;
+        table.resize(_capacity);
+        std::vector<std::list<std::pair<KeyType, ValueType>>> tempt;
+        tempt.resize(_capacity);
+	for (const auto& i: table)
+        {
+            for (const auto& j: i)
+            {
+                tempt[hash_function(j.first)].push_back(j);
+            }
+        }
+	table = std::move(tempt);
+    }
+    for (auto& i: table[hash_function(key)])
+    {
+	    if (i.first == key)
+	    {
+		    i.second = value;
+		    return;
+	    }
+    }
+    table[hash_function(key)].push_back({key, value});
+    ++_filled;
+}
+
+bool HashTable::find(const KeyType &key, ValueType &value) const
+{
+    for (const auto& i: table[hash_function(key)])
+    {
+        if ( i.first == key)
+        {
+	    value = i.second;
+	    return true;
         }
     }
     return false;
@@ -5,7 +65,7 @@
 
 void HashTable::remove(const KeyType &key)
 {
-    size_t size = table[hash_function(key)].size();
+    size_t size = table[hash_function(key)].size(); 
     table[hash_function(key)] = {};
     _filled -= size;
 }
@@ -15,10 +75,10 @@ ValueType& HashTable::operator[](const KeyType &key)
     size_t index = hash_function(key);
     for (auto& i: table[index])
     {
-            if (i.first == key)
-            {
-                    return i.second;
-            }
+	    if (i.first == key)
+	    {
+		    return i.second;
+	    }
     }
     return table[index].back().second;
 }
@@ -27,4 +87,3 @@ double HashTable::getLoadFactor()
 {
     return static_cast<double>(_filled)/_capacity;
 }
-"hashtable.cpp" [dos] 89L, 1886B                                                83,37         Bot
